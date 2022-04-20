ec7f3957859edafd6640ab17d7453b76b3b6e444
2nthony <ant.hu@outlook.com>
2022-04-21 02:14:41 +0800

    feat(18): result

--- error_handling/result/src/main.rs
+++ error_handling/result/src/main.rs
@@ -2,7 +2,8 @@ use std::num::ParseIntError;
 
 fn main() {
     // basic();
-    result_map();
+    // result_map();
+    early_return();
 }
 
 fn basic() {
@@ -21,7 +22,9 @@ fn basic() {
 }
 
 fn result_map() {
-    fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    type AliasedResult<T> = Result<T, ParseIntError>;
+
+    fn multiply(first_number_str: &str, second_number_str: &str) -> AliasedResult<i32> {
         match first_number_str.parse::<i32>() {
             Ok(first_number) => match second_number_str.parse::<i32>() {
                 Ok(second_number) => Ok(first_number * second_number),
@@ -54,4 +57,43 @@ fn result_map() {
 
     let tt = multiply_combination("t", "2");
     print(tt);
+}
+
+fn early_return() {
+    fn multiply(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+        let first_number = match first_number_str.parse::<i32>() {
+            Ok(n) => n,
+            Err(e) => return Err(e),
+        };
+        let second_number = match second_number_str.parse::<i32>() {
+            Ok(n) => n,
+            Err(e) => return Err(e),
+        };
+        Ok(first_number * second_number)
+    }
+
+    fn multiply_2(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+        let first_number = first_number_str.parse::<i32>()?;
+        let second_number = second_number_str.parse::<i32>()?;
+
+        Ok(first_number * second_number)
+    }
+
+    // deprecated try!
+    // fn multiply_try(first_number_str: &str, second_number_str: &str) -> Result<i32, ParseIntError> {
+    //     let first_number = try!(first_number_str.parse::<i32>());
+    //     let second_number = try!(second_number_str.parse::<i32>());
+
+    //     Ok(first_number * second_number)
+    // }
+
+    fn print(result: Result<i32, ParseIntError>) {
+        match result {
+            Ok(n) => println!("n is {}", n),
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+
+    print(multiply("10", "2"));
+    print(multiply_2("t", "2"));
 }
